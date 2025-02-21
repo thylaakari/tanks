5fed45bcab0d0801d518688e9d24f21f150a1b00
Lev Vorobev <thylaakari@yandex.ru>
2025-02-21 21:14:04 +0500

    outside walls

--- scripts/main.js
+++ scripts/main.js
@@ -24,26 +24,34 @@ document.body.addEventListener('keydown', (e) => {
 function goUp() {
   rotate(0)
   let n = removePx(getComputedStyle(player).top)
-  n -= 10
-  player.style.top = n + 'px'
+  if (n > 0) {
+    n = move(n, false)
+    player.style.top = n + 'px'
+  }
 }
 function goDown() {
   rotate(180)
   let n = removePx(getComputedStyle(player).top)
-  n += 10
-  player.style.top = n + 'px'
+  if (n < 450) {
+    n = move(n)
+    player.style.top = n + 'px'
+  }
 }
 function goLeft() {
   rotate(-90)
   let n = removePx(getComputedStyle(player).left)
-  n -= 10
-  player.style.left = n + 'px'
+  if (n > 0) {
+    n = move(n, false)
+    player.style.left = n + 'px'
+  }
 }
 function goRight() {
   rotate(90)
   let n = removePx(getComputedStyle(player).left)
-  n += 10
-  player.style.left = n + 'px'
+  if (n < 450) {
+    n = move(n)
+    player.style.left = n + 'px'
+  }
 }
 function shoot() {
   console.log('shoot')
@@ -55,4 +63,15 @@ function removePx(n) {
 
 function rotate(deg) {
   player.style.transform = `rotate(${deg}deg)`
+}
+
+function move(n, plus = true) {
+  if (plus) {
+    return (n += 10)
+  }
+  return (n -= 10)
+}
+
+function blockUp() {
+  console.log('block up')
 }
