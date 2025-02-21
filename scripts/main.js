0d773b756de3c12dc22ac982f2d3417bea91f01e
Lev Vorobev <thylaakari@yandex.ru>
2025-02-21 20:55:58 +0500

    rotate tank

--- scripts/main.js
+++ scripts/main.js
@@ -22,21 +22,25 @@ document.body.addEventListener('keydown', (e) => {
 })
 
 function goUp() {
+  rotate(0)
   let n = removePx(getComputedStyle(player).top)
   n -= 10
   player.style.top = n + 'px'
 }
 function goDown() {
+  rotate(180)
   let n = removePx(getComputedStyle(player).top)
   n += 10
   player.style.top = n + 'px'
 }
 function goLeft() {
+  rotate(-90)
   let n = removePx(getComputedStyle(player).left)
   n -= 10
   player.style.left = n + 'px'
 }
 function goRight() {
+  rotate(90)
   let n = removePx(getComputedStyle(player).left)
   n += 10
   player.style.left = n + 'px'
@@ -47,4 +51,8 @@ function shoot() {
 
 function removePx(n) {
   return +n.slice(0, -2)
+}
+
+function rotate(deg) {
+  player.style.transform = `rotate(${deg}deg)`
 }
